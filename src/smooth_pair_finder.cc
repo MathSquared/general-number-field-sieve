17b5b74986859fdc1fa83253d5aa0ae64f88b826
MathSquared <MathSquared@users.noreply.github.com>
2016-12-10 20:21:05 -0600

    Add SmoothPairFinder and constructor, with placeholder get

---
+++ src/smooth_pair_finder.cc
@@ -0,0 +1,52 @@
+#include "smooth_pair_finder.h"
+
+#include <NTL/RR.h>
+
+#include "util.h"
+
+namespace {
+    using namespace NTL;
+}
+
+namespace gnfs {
+    SmoothPairFinder::SmoothPairFinder(const ZZ& n, long B, const ZZX& f)
+      : n_(n), B_(B), f_(f), M_(B), Mold_(0), a_(B), b_(-1) {
+        ZZX fprime = diff(f);
+
+        // Figure out what the factorbase columns represent.
+        // Each one is a pair (p,r), where p is a prime up to B
+        // and r is something where f(r) \equiv 0 (mod p).
+        PrimeSeq pseq;
+        long p;
+        while ((p = pseq.next()) <= B) {
+            cols_modular_.push_back(p);
+            for (long r = 0; r < p; r++) {
+                ZZ res = eval(f, ZZ(r));
+                if (res % p == 0)
+                    cols_algebraic_.emplace_back(p, r);
+            }
+        }
+
+        // Generate pairs (q,s) for Adleman columns.
+        // For each q (named p here), find an s where
+        // f(s) \equiv 0 (mod p) and f'(s) \not\equiv 0 (mod p).
+        long k = conv<long>(3 * log(conv<RR>(n)));
+        long k_so_far = 0;
+        while (k_so_far < k) {
+            for (long s = 0; s < p; s++) {
+                ZZ zs(s);
+                if (eval(f, zs) % p == 0 && eval(fprime, zs) % p != 0) {
+                    cols_adleman_.emplace_back(p, s);
+                    k_so_far++;
+                }
+            }
+
+            p = pseq.next();
+        }
+    }
+
+    std::pair<std::pair<long, long>, vec_GF2> SmoothPairFinder::get() {
+        // TODO
+        return std::make_pair(std::make_pair(0, 0), vec_GF2(INIT_SIZE, num_cols()));
+    }
+}
---
+++ src/smooth_pair_finder.h
@@ -0,0 +1,58 @@
+#ifndef _SMOOTH_PAIR_FINDER_H_
+#define _SMOOTH_PAIR_FINDER_H_
+
+#include <vector>
+
+#include <NTL/vec_GF2.h>
+#include <NTL/ZZ.h>
+#include <NTL/ZZX.h>
+
+namespace gnfs {
+    // Produces rows of a matrix in GF2 corresponding to pairs a,b, where
+    // - the ideal (a + \alpha b) is B-smooth and
+    // - a + mb is B-smooth mod n.
+    class SmoothPairFinder {
+      public:
+        // Creates an apparatus to find B-smooth pairs and exponent vectors
+        // with parameters n and f.
+        SmoothPairFinder(const NTL::ZZ& n, long B, const NTL::ZZX& f);
+
+        // Returns the number of columns in an exponent vector
+        // returned from this apparatus.
+        long num_cols() {
+            return cols_modular_.size() + cols_algebraic_.size()
+                + cols_adleman_.size();
+        }
+
+        // Produces a smooth exponent vector,
+        // returning a, b, and the exponent vector.
+        std::pair<std::pair<long, long>, NTL::vec_GF2> get();
+      private:
+        const NTL::ZZ& n_;
+        const long B_;
+        const NTL::ZZX& f_;
+
+        // Size of the region we're currently searching.
+        // a_ and b_ range over [-M,M]\[-Mold,Mold] x (Mold,M].
+        // (This intentionally excludes a=0 when Mold=0.)
+        const long M_;
+        // If we had to expand M_, this is the size of the region
+        // that we'd already examined.
+        // Starts at 0.
+        const long Mold_;
+        // Cursors for the search. These indicate where we last left off.
+        // Each request of a row starts at the pair after (a,b),
+        // in b-major order.
+        const long a_;
+        const long b_;
+
+        // The entries in returned exponent vectors represent
+        // modular factorbase entries, algebraic factorbase entries,
+        // or Adleman columns.
+        std::vector<long> cols_modular_;
+        std::vector<std::pair<long, long>> cols_algebraic_;  // pairs (p,r)
+        std::vector<std::pair<long, long>> cols_adleman_;  // pairs (q,s)
+    };
+}
+
+#endif
